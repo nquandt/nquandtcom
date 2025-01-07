2bc0ef312291a14acf0f0f4c40aefee93ae77816
Nicholas Quandt <57432850+nquandt@users.noreply.github.com>
2025-01-06 22:34:48 -0600

    add updated bullets

--- src/Server/Views/Components/Resume/Jobs/Consultant.cs
+++ src/Server/Views/Components/Resume/Jobs/Consultant.cs
@@ -3,14 +3,16 @@ namespace Components.Resume.Jobs;
 
 public class Consultant : JobModel
 {
+    public override string DateRange => "Various 6-12 month contracts";
     public override string Title => "Software Consultant";
     public override string[] Bullets => [
-        "Migrate Mulesoft Flows to .NET Web API's incrementally by endpoint.",
-        "Build .NET API wrappers around legacy soap services to utilize REST + JSON instead of XML.",
-        "Create a shared package for IOptions pattern validation in ASP.NET applications.",
-        "Utilized .NET 6/8, ASP.NET, XUnit for testing, NSubstitute for mocking, FluentValidation for model validation and configuration validation.",
-        "Drive discussion and direction around modern .NET architecture and practices.",
-        "Participate in standard Agile practice, such as planning, review, retrospectives, PI planning.",
-        "Utilize github for repos, including workflows for deployment.",
+"Led an incremental migration of Mulesoft flows to .NET Web APIs, implementing a phased approach by endpoint to ensure seamless integration and minimal disruption to business operations.",
+"Designed and developed .NET API wrappers around legacy SOAP services, modernizing interfaces by transitioning to RESTful standards with JSON payloads, improving usability and compatibility.",
+"Created and deployed a shared .NET package to enforce validation using the IOptions pattern, enhancing configuration consistency and reducing runtime errors in ASP.NET applications.",
+"Leveraged XUnit for robust testing, incorporating NSubstitute for mocking and FluentValidation for comprehensive model and configuration validation.",
+"Facilitated discussions and provided strategic guidance on modern .NET architecture and development practices, aligning teams with industry standards and best practices.",
+"Introduced the use of Azure Key Vault in ASP.NET applications for secure management of application secrets and certificates.",
+"Collaborated with development teams to deploy containerized applications using Azure Kubernetes Service, optimizing scalability and operational efficiency.",
+"Implemented Github Actions in various repositories for NuGet publishing and application containerization."
     ];
 }
--- src/Server/Views/Components/Resume/Jobs/MilwaukeeTool.cs
+++ src/Server/Views/Components/Resume/Jobs/MilwaukeeTool.cs
@@ -10,20 +10,18 @@ public class MilwaukeeTool : JobModel
 
     public override string Url => "https://www.milwaukeetool.com";
     public override string[] Bullets => [
-        "Maintain a multi-site [Sitecore 10 XM](https://www.sitecore.com/products/experience-manager?utm_websource=products) topology in kubernetes ([AKS](https://docs.microsoft.com/en-us/azure/aks/intro-kubernetes)), with full server side rendering via. [Razor](https://docs.microsoft.com/en-us/aspnet/core/mvc/views/razor?view=aspnetcore-6.0).",
-        "Scope and initiate an incremental migration from [Razor](https://docs.microsoft.com/en-us/aspnet/core/mvc/views/razor?view=aspnetcore-6.0) pages to [NextJS](https://nextjs.org/) as our primary application front end. This migration aims to adopt a headless philosophy by seperating our CMS from our rendering host.",
-        "Incrementally migrate backend services to .NET 6/7/8 from .NET Framework 4.8. Utilize path based routing to orchestrate between boundaries.",
-        "Host applications following multi-region practices. Basic horizontal scalability, statelessness, eventing. Utilize a combinination of [AKS](https://docs.microsoft.com/en-us/azure/aks/intro-kubernetes) and [Azure Container Apps](https://azure.microsoft.com/en-us/products/container-apps) with event grid events triggering key vault or configuration changes/reloads. [Traffic Manager](https://learn.microsoft.com/en-us/azure/traffic-manager/traffic-manager-overview) -> [Application Gateway](https://learn.microsoft.com/en-us/azure/application-gateway/overview) -> Container Environments.",
-        "Restructuring of our content system to align closer to our coding mental models. Templates -> Models, Content -> Class Instances etc, allowing for faster development cycles.",
-        "Build and maintain a shared repository to standardize .NET middlewares, logging, options patterns, health checks and more in our multiservice topology.",
-        "Implement [Terraform](https://www.terraform.io/) as our IaC tool, using Azure Storage Accounts as a backend provider to keep tooling in our Azure cloud.",
-        "Utilize [YARP](https://github.com/microsoft/reverse-proxy) as a reverse proxy to route traffic between multiple applications. Included cookie based authentication middleware with redis sessions (effectively a BFF layer).",
-        "Plan and execute the back-end half of development for a [product catalog utility](https://www.milwaukeetool.com/innovations/solutions), and [product comparison tool](https://www.milwaukeetool.com/products/power-tools/drilling/compare), both powered by [Svelte](https://svelte.dev/) and [Solr](https://solr.apache.org/).",
-        "Create a new solution to decouple a small team from the primary development team to allow rapid development utilizing a microservice architecture. Including full devops pipelines and procedures.",
-        "Implement a new development workflow for a small offshoot web project, utilizing [Gulp](https://gulpjs.com/) and [Rollup](https://rollupjs.org/).",
-        "Integrate our .NET applications with [Azure Key Vault](https://docs.microsoft.com/en-us/azure/key-vault/general/basic-concepts) for key, secret, and certificate maintainance and security.",
-        "Strategize and execute a site wide migration from Brightcove to Vimeo as a video hosting platform, including library migration.",
-        "Implement and standardize health checks in our microservice applications to prevent noticed cold starts and downtime on kubernetes.",
-        "Give monthly lunch'n'learn talks to the greater web dev team on various topics including: general development practice, new technologies, coding principles, etc.",
+        "Discern architectural direction and implementation details for www.milwaukeetool.com and corresponding digital products, with a focus on engaging users through cohesive and branded experiences.",
+        "Deliver business-critical features by aligning technical solutions with organizational goals, ensuring scalability and maintainability.",
+        "Drive mentalities away from monolithic approaches leaning into the idealogy that digital products can be isolated plugins, effectively vertical slice architecture at a product level.",
+        "Scope and initiate an incremental migration to React-SSR as a front end rendering host from .NET Framework 4.8 MVC, enabling new hiring opportunies, improved development experiences, and an expanded tooling ecosystem for user focused products. This project spanned 18 months, including 40,000+ pages, and nearly 500 unique views. Migration allowed the reduction to 74 views and improved brand cohesion across digital assets through the use of a shared design component library.",
+        "Pilot a shared React design component library for use in 6 different products initially, following an atomic design philosophy.",
+        "Restructure monolithic application services into microservices specific to individual business verticals, allowing for faster feedback loops on development cycles. The monolith averaged 75 minutes to build/deploy, while each individual service ranges 1-8 minutes.",
+        "Maintain a consistent development experience aimed at scaling products consistently and efficiently through the creation of an interal application framework. The framework streamlines aspnetcore application setup for Key Vault and App Configuration setup, including dynamic configuration updates via Event Grid eventing.",
+        "Standardize application deployment and hosting through the use of Azure Devops Pipelines, Azure Container Registry, Azure Kubernetes Service and Azure Container Apps.",
+        "Utilize YARP as a reverse proxy to route traffic between applications, incorporating cookie-based authentication middleware with Redis sessions to support a scaled Backend-for-Frontend (BFF) architecture. Doubles as an inverse application registration layer allowing A/B testing or full replacement of digital products without interuptions, consistent with a composable strategy.",
+        "Monitor Azure resources using New Relic via an OTLP (OpenTelemetry) endpoint, enabling comprehensive observability and streamlined performance monitoring.",
+        "Implement Terraform for Infrastructure as Code (IaC), utilizing Azure Storage Accounts as the backend provider",
+        "Approach products from an Agile perspective, constantly iterating based on metrics on user engagement. Speed, data consistency, and brand alignment being key focus points.",
+        "Deliver monthly lunch-and-learn sessions for the broader web development team, presenting on topics such as modern development practices, emerging technologies, and coding principles to foster professional growth and innovation."
     ];
 }
